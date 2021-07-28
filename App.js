eddc3e0fc4787a667d28165e9c64ae7ef4f9b2d0
dave 1978 <dawitlemm88@gmail.com>
2021-07-28 05:13:59 +0300

    custom-drawer

--- App.js
+++ App.js
@@ -9,9 +9,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import MainTabScreen from './screens/MainTabScreen'
+// import { createStackNavigator } from '@react-navigation/stack';
 // import Icon from 'react-native-vector-icons/Ionicons';
 // import { Icon } from 'react-native-elements';
 
@@ -22,6 +21,13 @@ import {
   Button,
   View,
 } from 'react-native';
+
+import { DrawerContent } from './screens/DrawerContent'
+
+import MainTabScreen from './screens/MainTabScreen'
+import MapScreen from './screens/MapScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import SupportScreen from './screens/SupportScreen'
 
 // import HomeScreen from './screens/HomeScreen';
 // import DetailsScreen from'./screens/DetailsScreen';
@@ -47,11 +53,14 @@ const Drawer = createDrawerNavigator();
 const App = () =>{
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+       
+      <Drawer.Navigator drawerContent={props  => <DrawerContent{ ... props}/>} initialRouteName='Home'>
         <Drawer.Screen name="Home" component={MainTabScreen} />
-        {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
+        <Drawer.Screen name="MapScreen" component={MapScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
       </Drawer.Navigator>
-      {/*  */}
+    
     </NavigationContainer>
   );
 }
---
+++ screens/MapScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const MapScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Map Screen</Text>
+        <Button
+          title="Click Here"
+          onPress={() => alert('Button Clicked!')}
+        />
+      </View>
+    );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
